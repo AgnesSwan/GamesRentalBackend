472b194a02c496a272d16b813131bb834096b551
Agnieszka Szmidtke <agnes.swan7@gmail.com>
2021-01-26 22:06:48 +0100

    delete checkAuth in post

--- server/api/routes/games.js
+++ server/api/routes/games.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const GamesController = require('../../controllers/games');
 const checkAuth = require('../middleware/check-auth');
 router.get('/', GamesController.games_get_all);
-router.post('/', checkAuth, GamesController.games_new);
+router.post('/',  GamesController.games_new);
 router.patch('/:gameId', checkAuth, GamesController.game_update);
 router.delete('/:gameId', checkAuth, GamesController.game_delete);
 module.exports = router;
